14021510539c552671692cdcf42eaa944d87a2a6
Baozhu Zuo <zuobaozhu@gmail.com>
2022-03-01 09:31:19 +0800

    modules: yolo.py insert swift nn.modules

--- models/yolo.py
+++ models/yolo.py
@@ -17,6 +17,7 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 # ROOT = ROOT.relative_to(Path.cwd())  # relative
 
+from models.swift_common import *
 from models.common import *
 from models.experimental import *
 from utils.autoanchor import check_anchor_order
@@ -257,7 +258,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
 
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in [Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
-                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost]:
+                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, conv_bn_relu_maxpool, ShuffleNetV2_InvertedResidual]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
