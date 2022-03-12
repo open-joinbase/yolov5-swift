425098c902b9fff939ae104eed7ee66ad1628e95
Baozhu Zuo <zuobaozhu@gmail.com>
2022-03-12 15:01:14 +0800

    models: ShuffleNetV2 use the network name

--- models/swift_common.py
+++ models/swift_common.py
@@ -18,22 +18,24 @@ def channel_shuffle(x: Tensor, groups: int) -> Tensor:
     return x
 
 
-class conv_bn_relu_maxpool(nn.Module):
-    def __init__(self, c1, c2,shortcut=True, g=1, e=0.5):  # ch_in, ch_out
-        super(conv_bn_relu_maxpool, self).__init__()
-        self.cv1=Conv(c1,c2,3,2,act=nn.ReLU(True))
+# conv bn relu maxpool
+class BMConv(nn.Module):
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out
+        super(BMConv, self).__init__()
+        self.cv1 = Conv(c1, c2, 3, 2, act=nn.ReLU(True))
         self.m = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
 
     def forward(self, x):
         return self.m(self.cv1(x))
 
 
-class ShuffleNetV2_InvertedResidual(nn.Module):
+# ShuffleNetV2 Inverted Residual Neural Network
+class ShuffleNetV2(nn.Module):
     def __init__(
             self,
             c1, c2, stride,
     ) -> None:
-        super(ShuffleNetV2_InvertedResidual, self).__init__()
+        super(ShuffleNetV2, self).__init__()
 
         if not (1 <= stride <= 3):
             raise ValueError('illegal stride value')
@@ -43,17 +45,16 @@ class ShuffleNetV2_InvertedResidual(nn.Module):
         assert (self.stride != 1) or (c1 == branch_features << 1)
 
         if self.stride > 1:
-            self.cv1=Conv(c1,c1,3,self.stride,act=nn.ReLU(True),g=c1)
-            self.cv2=Conv(c1,branch_features,1,1)
+            self.cv1 = Conv(c1, c1, 3, self.stride, act=nn.ReLU(True), g=c1)
+            self.cv2 = Conv(c1, branch_features, 1, 1)
         else:
             self.cv1 = nn.Sequential()
             self.cv2 = nn.Sequential()
 
         self.m = nn.Sequential(
-            Conv(c1 if (self.stride > 1) else branch_features,branch_features,1,1),
-            Conv(branch_features, branch_features,3,self.stride,1,g=branch_features),
+            Conv(c1 if (self.stride > 1) else branch_features, branch_features, 1, 1),
+            Conv(branch_features, branch_features, 3, self.stride, 1, g=branch_features),
             Conv(branch_features, branch_features, 1, 1))
-
 
     def forward(self, x: Tensor) -> Tensor:
         if self.stride == 1:
@@ -65,4 +66,3 @@ class ShuffleNetV2_InvertedResidual(nn.Module):
         out = channel_shuffle(out, 2)
 
         return out
-
