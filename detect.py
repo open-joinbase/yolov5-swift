5ed8a8df4eeee0bfb185c726fbc0c0ce38109e39
mjq2020 <mjqx2020@gmail.com>
2022-04-20 11:24:51 +0800

    delete post-processing method

--- detect.py
+++ detect.py
@@ -125,10 +125,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         dt[1] += t3 - t2
 
         # NMS
-        if pred.shape[-1] == 6:
-            pred = [detection_no_object(pred)]
-        else:
-            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
 
         dt[2] += time_sync() - t3
 
